2606846a2e01efc80bd51e2457acd89a03763f15
Leonardo Raimundo <leonardo.rraimundo95@gmail.com>
2022-07-08 10:42:46 -0300

    added javascript and images with flask static files. Used databases with SQLAlchemy, but it's not working yet.

--- hello.py
+++ hello.py
@@ -1,12 +1,28 @@
-from flask import Flask, render_template
+from flask import Flask, flash, render_template
 from flask_wtf import FlaskForm #framework for creating forms.
 from wtforms import StringField, SubmitField #Input box and submit button.
 from wtforms.validators import DataRequired #to validate wether input has been passed.
+from flask_sqlalchemy import SQLAlchemy #import database.
+from datetime import datetime #import current time.
 
 #Create a Flask Instane
 app = Flask(__name__) #helps Flask find our files on the directory
-
+#Add Database.
+app.config['SQLAlchemy_DATABASE_URI'] = 'sqlite:///users.db'
+#Secret Key.
 app.config['SECRET_KEY'] = "my super secret key that no one is supposed to know" #security measure for working with whatheforms.
+
+db = SQLAlchemy(app)
+#Create a Model.
+class Users(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    name = db.Column(db.String(200), nullable=False)
+    email = db.Column(db.String(120), nullable=False, unique=True)
+    date_added = db.Column(db.DateTime, default=datetime.utcnow)
+
+    #Create a String.
+    def __repr__(self):
+        return '<Name %r>' % self.name
 
 #Create a Form Class
 class NamerForm(FlaskForm):
@@ -30,10 +46,9 @@ class NamerForm(FlaskForm):
 
 #returning index.html from templates folder.
 def index():
-    first_name = "John"
+    first_name = "Leonardo"
     #stuff = "This is <strong>Bold Text</strong>"
     stuff = "This is bold text"
-
     favorite_pizza = ["Pepperoni", "Cheese", "Musshroom", 41]
     return render_template("index.html", 
     first_name=first_name,
@@ -71,7 +86,9 @@ def name():
     #Validade Form
     if form.validate_on_submit(): #if someone submits a name, replace name = None with whatever name they passed.
         name = form.name.data
-        form.name.date = ''
+        form.name.data = ''
+        flash("Form Submitted Successfully!")
+
     return render_template("name.html",
         name = name,
         form = form)
