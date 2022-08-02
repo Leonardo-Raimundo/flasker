779335c797ed9bcd7a8c0b4d527ba9df4e5a33ac
Leonardo Raimundo <leonardo.rraimundo95@gmail.com>
2022-08-02 10:58:52 -0300

    #25 done, not working

--- hello.py
+++ hello.py
@@ -75,6 +75,31 @@ def logout():
 @app.route('/dashboard', methods=['GET', 'POST'])
 @login_required
 def dashboard():
+    form = UserForm()
+    id = current_user.id
+    name_to_update = Users.query.get_or_404(id)
+    if request.method == "POST":
+        name_to_update.name = request.form['name']
+        name_to_update.email = request.form['email']
+        name_to_update.favorite_color = request.form['favorite_color']
+        name_to_update.username = request.form['username']
+
+        try:
+            db.session.commit()
+            flash("User Updated Successfully!")
+            return render_template("dashboard.html", 
+                form=form,
+                name_to_update=name_to_update, id = id)
+        except:
+            flash("Error! Looks like there was a problem...try again.")
+            return render_template("dashboard.html", 
+                form=form,
+                name_to_update=name_to_update)
+    else:
+        return render_template("dashboard.html", 
+                form=form,
+                name_to_update=name_to_update, id = id)
+    
     return render_template('dashboard.html')
 
 #Create a Blog Post Model
@@ -250,6 +275,7 @@ def update(id):
         name_to_update.name = request.form['name']
         name_to_update.email = request.form['email']
         name_to_update.favorite_color = request.form['favorite_color']
+        name_to_update.username = request.form['username']
 
         try:
             db.session.commit()
